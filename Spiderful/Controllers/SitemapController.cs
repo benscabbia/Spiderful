4db675c8ea0673fdd424575c615e3c9ee9b0605f
Benjamin Scabbia <benscabbia@gmail.com>
2016-08-17 07:28:03 +0100

    Added sitemap controller

---
+++ Spiderful/Controllers/SitemapController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Spiderful.Controllers
+{
+    public class SitemapController : ApiController
+    {
+        // GET api/sitemap
+        public IEnumerable<string> Get()
+        {
+            //todo really, probably return usage page?
+            return new string[] { "link1", "link2" };
+        }
+
+        // GET api/sitemap/5
+        public string Get(int id)
+        {
+
+
+ 
+            //string a = "www.google.com";
+            //bool b = testc("google.com");
+            string a = "http://google.com";
+            //bool d = testc("http://google.com");
+            //bool e = testc("lanext.it");
+
+            Uri uriResult;
+            bool result = Uri.TryCreate(a, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            return "result: " + result;
+        }
+
+        // POST api/sitemap
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/sitemap/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/sitemap/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
