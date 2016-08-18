ebea73d34583c9fcc569bad507471aa162fe63c0
Benjamin Scabbia <benscabbia@gmail.com>
2016-08-18 07:00:43 +0100

    URL with params can now be passed to controller

--- Spiderful/Controllers/SitemapController.cs
+++ Spiderful/Controllers/SitemapController.cs
@@ -16,20 +16,23 @@ namespace Spiderful.Controllers
             return new string[] { "link1", "link2" };
         }
 
-        // GET api/sitemap/5
-        public string Get(int id)
+        // GET api/sitemap/5 if Get(int id)
+        // GET api/sitemap?url=http://www.google.com for below
+        //    /api/sitemap?url=http://www.google.com/index.net&level=15&isOnSite=false
+        public string Get(string url, int level=0, bool isOnSite=true)
         {
 
+            //
 
  
             //string a = "www.google.com";
             //bool b = testc("google.com");
-            string a = "http://google.com";
+            //string a = "http://google.com";
             //bool d = testc("http://google.com");
             //bool e = testc("lanext.it");
 
             Uri uriResult;
-            bool result = Uri.TryCreate(a, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
 
             return "result: " + result;
         }
