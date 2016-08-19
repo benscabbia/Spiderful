40ab436bf62c21d770be4720c1811860b4d30dec
Benjamin Scabbia <benscabbia@gmail.com>
2016-08-19 07:45:29 +0100

    Added UrlService Added HtmlAgilityPack

--- Spiderful/Controllers/SitemapController.cs
+++ Spiderful/Controllers/SitemapController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using Spiderful.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -22,19 +24,53 @@ namespace Spiderful.Controllers
         public string Get(string url, int level=0, bool isOnSite=true)
         {
 
-            //
+            //class source
+            //funct(url, n, bool)
+            //service.checkandform(url)
+            //if (url is ok)
+            //list = service.getallpages(url)
 
- 
+            //Uri uriResult;
+            //bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            //List<string> hrefTags = new List<string>();
+            //if (result)
+            //{
+
+            //    HtmlWeb hw = new HtmlWeb();
+            //    HtmlDocument doc = hw.Load(url);
+            //    foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            //    {
+            //        HtmlAttribute att = link.Attributes["href"];
+            //        if (!String.IsNullOrEmpty(att.Value))
+            //        {
+            //            hrefTags.Add(att.Value);
+            //        }
+                    
+            //    }
+
+            //    foreach(var a in hrefTags)
+            //    {
+            //        System.Diagnostics.Debug.WriteLine(a.ToString());
+            //    }
+
+            //}
+
+
+            return "result: " + UrlService.isValidSmart(url);
+
+            //http://stackoverflow.com/questions/2248411/get-all-links-on-html-page
+            //https://htmlagilitypack.codeplex.com/downloads/get/120935#
+            //https://htmlagilitypack.codeplex.com/
+            //https://www.spotify.com/ie/legal/new-60-days-free-trial-terms-and-conditions/
+
+
             //string a = "www.google.com";
             //bool b = testc("google.com");
             //string a = "http://google.com";
             //bool d = testc("http://google.com");
             //bool e = testc("lanext.it");
 
-            Uri uriResult;
-            bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
 
-            return "result: " + result;
         }
 
         // POST api/sitemap
---
+++ Spiderful/Models/UrlService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace Spiderful.Models
+{
+    public static class UrlService
+    {
+        /// <summary>
+        /// A method used to determine whether a URL is valid. The URL must use a scheme
+        /// Valid URL's include: http://test.com, https://www.test.com. http://test.com/page1/page2, etc
+        /// </summary>
+        /// <param name="url">A string representing the URL</param>
+        /// <returns>A boolean determining if the URL is valud</returns>
+        public static bool isValid(string url)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+        /// <summary>
+        /// A method used to determine whether a URL is valid, regardless if it has a scheme. 
+        /// Valid URL's include: www.test.com, test.ocm, test.com/page1/page2 and all of isValid
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool isValidSmart(string url)
+        {
+            if (isValid(url))
+            {
+                return true;
+            }
+            else
+            {
+
+                UriBuilder link = new UriBuilder(url);
+                return isValid(link.Uri.ToString());
+            }
+        }
+    }
+}
+             
+ 
+//        Uri uriResult;
+//        bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+//        List<string> hrefTags = new List<string>();
+//            if (result)
+//            {
+
+//                HtmlWeb hw = new HtmlWeb();
+//        HtmlDocument doc = hw.Load(url);
+//                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+//                {
+//                    HtmlAttribute att = link.Attributes["href"];
+//                    if (!String.IsNullOrEmpty(att.Value))
+//                    {
+//                        hrefTags.Add(att.Value);
+//                    }
+
+//}
+
+//                foreach(var a in hrefTags)
+//                {
+//                    System.Diagnostics.Debug.WriteLine(a.ToString());
+//                }
+//    }
+//}
